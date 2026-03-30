ebabc8ccc30d0326a325e660bd6ccc9b982152bb
quentinltg <quentint2005@gmail.com>
2026-03-30 16:24:39 +0200

    fix(#28): Fix du userId (WIP)

--- src/main/java/fr/univartois/butinfo/sae/abyss/social/mapper/PostMapper.java
+++ src/main/java/fr/univartois/butinfo/sae/abyss/social/mapper/PostMapper.java
@@ -4,13 +4,19 @@ import fr.univartois.butinfo.sae.abyss.social.dto.PostDTO;
 import fr.univartois.butinfo.sae.abyss.social.model.Post;
 import jakarta.validation.Valid;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
+
     PostDTO toDTO(Post post);
+
     List<PostDTO> toDTOs(List<Post> posts);
+
+    @Mapping(target = "user", ignore = true)
     Post toEntity(@Valid PostDTO postDTO);
+
 
 }
