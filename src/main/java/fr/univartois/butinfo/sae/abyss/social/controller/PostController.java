92445e081ca3d9a31ed52d011392ac474434f921
quentinltg <quentint2005@gmail.com>
2026-03-30 16:23:54 +0200

    fix(#31): Passage de la route au pluriel

--- src/main/java/fr/univartois/butinfo/sae/abyss/social/controller/PostController.java
+++ src/main/java/fr/univartois/butinfo/sae/abyss/social/controller/PostController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/posts")
 public class PostController {
     private final PostService postService;
     private final PostMapper postMapper;
