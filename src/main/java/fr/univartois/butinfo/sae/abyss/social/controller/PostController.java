66d175bbfc0f83c0c739d891ccfec90f50aec45c
quentinltg <quentint2005@gmail.com>
2026-03-30 15:49:16 +0200

    feat(#31): Création du controller pour les posts

---
+++ src/main/java/fr/univartois/butinfo/sae/abyss/social/controller/PostController.java
@@ -0,0 +1,36 @@
+package fr.univartois.butinfo.sae.abyss.social.controller;
+
+import fr.univartois.butinfo.sae.abyss.social.dto.PostDTO;
+import fr.univartois.butinfo.sae.abyss.social.mapper.PostMapper;
+import fr.univartois.butinfo.sae.abyss.social.model.Post;
+import fr.univartois.butinfo.sae.abyss.social.service.PostService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+    private final PostService postService;
+    private final PostMapper postMapper;
+
+    public PostController(PostService postService, PostMapper postMapper) {
+        this.postService = postService;
+        this.postMapper = postMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDTO postDTO) {
+        Post post = postMapper.toEntity(postDTO);
+        Post savedPost = postService.save(post);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(postMapper.toDTO(savedPost));
+
+    }
+
+}
+
