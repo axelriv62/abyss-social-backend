a47047418cc830ae14f6e8b967b3f5908c00a867
axel <axel_riviere@ens.univ-artois.fr>
2026-04-01 10:18:12 +0200

    fix(#2): correction d'imports

--- src/main/java/fr/univartois/butinfo/sae/abyss/social/controller/UserController.java
+++ src/main/java/fr/univartois/butinfo/sae/abyss/social/controller/UserController.java
@@ -9,9 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 /**
