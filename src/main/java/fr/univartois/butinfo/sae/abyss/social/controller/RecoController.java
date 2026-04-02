ec926e69d4e1424e3f27b8f5b058e26404f3a20f
Romain <romain_notteau@ens.univ-artois.fr>
2026-04-02 19:37:54 +0200

    fix : correction de la route pour les recos

--- src/main/java/fr/univartois/butinfo/sae/abyss/social/controller/RecoController.java
+++ src/main/java/fr/univartois/butinfo/sae/abyss/social/controller/RecoController.java
@@ -1,8 +1,10 @@
 package fr.univartois.butinfo.sae.abyss.social.controller;
 
+import fr.univartois.butinfo.sae.abyss.social.model.User;
 import fr.univartois.butinfo.sae.abyss.social.service.RecommendationService;
 import fr.univartois.butinfo.sae.abyss.social.dto.UserRecommendedDTO;
 import org.bson.types.ObjectId;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,8 +18,8 @@ public class RecoController {
         this.recommendationClientService = recommendationClientService;
     }
 
-    @GetMapping("/users/{userId}")
-    public List<UserRecommendedDTO> getRecommendedUsers(@PathVariable ObjectId userId) {
-        return recommendationClientService.getUserRecommendations(userId);
+    @GetMapping("/users")
+    public List<UserRecommendedDTO> getRecommendedUsers(@AuthenticationPrincipal User currentUser) {
+        return recommendationClientService.getUserRecommendations(currentUser.getId());
     }
 }
