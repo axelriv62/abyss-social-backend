ce7eacefdf43b5bbeff46897f625c6f9992502bb
axel <axel_riviere@ens.univ-artois.fr>
2026-04-27 14:26:56 +0200

    feat: oublie dans la config de Cors

--- src/main/java/fr/univartois/butinfo/sae/abyss/social/config/SecurityConfig.java
+++ src/main/java/fr/univartois/butinfo/sae/abyss/social/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import fr.univartois.butinfo.sae.abyss.social.service.LogoutService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -64,6 +65,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authenticationProvider(authenticationProvider)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(r -> r
